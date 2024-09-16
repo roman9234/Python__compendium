6fbf2dc797478e67ceccf8e233ef4cc356dfef9d
roman9234 <roman_lesovoy@mail.ru>
2024-09-16 23:56:42 +0300

    added more code samples

--- coding_principles/functional_programming/info/4_lambda_functions.py
+++ coding_principles/functional_programming/info/4_lambda_functions.py
@@ -9,17 +9,22 @@
 # Синтаксис:
 # lambda arguments: expression
 
-double = lambda x: x*2
+double = lambda x: x * 2
 print(double(2))
 
-mylist = [2,20,5,26,35,17,34]
+mylist = [2, 20, 5, 26, 35, 17, 34]
 # lambda как параметр функции высшего порядка
-filtered_list = list(filter(lambda x: (x%2 == 0), mylist))
+filtered_list = list(filter(lambda x: (x % 2 == 0), mylist))
 print(filtered_list)
 
+# складываение чисел
+add = lambda x, y: x + y
+print(add(1, 3))
 
-
-
+# сортировка списка кортежей (по 2 элементу)
+points = [(1, 2), (3, 1), (5, 4), (2, 2)]
+points_sorted = sorted(points, key=lambda x: x[1])
+print(points_sorted)
 
 
 
